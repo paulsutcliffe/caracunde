018ba1df306cc44d59ed3c6ab8aa4f53282a1650
Paul Sutcliffe <paul@kosmyka.com>
2012-08-29 17:54:30 -0500

    Fixed errors, all tests green

--- features/step_definitions/integrantes_steps.rb
+++ features/step_definitions/integrantes_steps.rb
@@ -1,7 +1,6 @@
 Quando /^preencho os campos e adiciono uma foto$/ do
   fill_in("Nome", :with => "Paul")
   fill_in("Descrição", :with => "Estudo um ano de piano erudito")
-  fill_in("País", :with => "Peru")
   path = "features/support/picture.jpg"
   attach_file("Foto", File.expand_path(path))
 end
@@ -9,7 +8,7 @@ end
 Então /^deveria ver a mesma informação que preenchei$/ do
   page.should have_content("Paul")
   page.should have_content("Estudo um ano de piano erudito")
-  page.should have_content("Peru")
+  page.should have_content("peru")
   page.should have_xpath("//img[contains(@src, \"picture.jpg\")]")
 end
 
